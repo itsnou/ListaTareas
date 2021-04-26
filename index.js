edc6751d30bb60e036aa0dd333c13b8640bf21aa
itsnou <emiliano.meneses@gmail.com>
2021-04-26 18:17:41 -0300

    Lista de tareas

---
+++ index.js
@@ -0,0 +1,61 @@
+toDoItems = [];
+
+let span = document.querySelector("#createdBy");
+span.innerHTML= "Aplicación creada por Emiliano"
+
+class ToDo {
+    constructor(description) {
+        this.description = description;
+        this.complete = false;
+    }
+    completeToDo() {
+        this.complete = true;
+    }
+}
+
+function buildToDo(todo, index) {
+    let toDoShell = document.createElement('div');
+    toDoShell.setAttribute("class","toDoShell");
+    let toDoText = document.createElement('span');
+    toDoText.innerHTML= todo.description;
+    toDoText.setAttribute("id", index);
+    if(todo.complete === true){
+      toDoText.setAttribute('class','completeText');
+      
+    };
+    toDoShell.appendChild(toDoText);
+    toDoText.addEventListener("click", completeToDo);
+    return toDoShell;
+}
+
+function buildToDos(toDos) {
+    let array=toDos.map(buildToDo);
+    return array;
+}
+
+function displayToDos() {
+    let toDoContainer = document.querySelector('#toDoContainer');
+    toDoContainer.innerHTML= "";
+    let build = buildToDos(toDoItems);
+    for(let i = 0; i<build.length; i++){
+      toDoContainer.appendChild(build[i]);
+    }
+    
+}
+
+function addToDo() {
+    let input = document.querySelector('#toDoInput');
+    let toDo = new ToDo(input.value);
+    toDoItems.push(toDo);
+    input.value = '';
+    displayToDos();
+}
+
+
+document.querySelector('#addButton').addEventListener('click', addToDo);
+
+function completeToDo(event) {
+    const index = event.target.id;
+    toDoItems[index].completeToDo();
+    displayToDos();
+}
